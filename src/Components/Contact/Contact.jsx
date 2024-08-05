8e6e15e979df40ee58f094725dcda37fad82d160
rraawww10 <102401151+rraawww10@users.noreply.github.com>
2024-08-05 12:24:20 +0530

    Add files via upload

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "7a5e9e29-f283-407b-bfd2-d2496774dcc8");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+  
+
+
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <p>Feel free to reach out through contact form or find our contact information below.
+                Your feedback, suggestions and questions are important to us as we strive to provide 
+                exceptional service to our university community.
+            </p>
+            <ul>
+                <li> <img src={mail_icon} alt="" />rajsinghrishu507@gmail.com</li>
+                <li><img src={phone_icon} alt="" />+91 9564511200</li>
+                <li><img src={location_icon} alt="" />Nirman Hardware ,Malmaliya road, Taxi Stand-Beside HANUMAN Mandir</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your Name</label>
+                <input type="text"name='name' placeholder='Enter Your Name' required />
+
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter Mobile' required />
+
+                <label>Write Your Message Here</label>
+                <textarea name="message" rows="6" placeholder='Enter Your Message' required></textarea>
+                <button type='submit' className='btn dark-btn'>Bhej Do <img src={white_arrow} alt="" /></button>
+            </form>
+            <span>{result}</span>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @2024. All rights reserved</p>
+        <ul>
+            <li>Terms Of Service</li>
+            <li>Privacy Policy</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Footer
